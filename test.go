b200097d2053b945766db46ed24dd19f77c83241
RuiO <139987905+cx-rui-oliveira@users.noreply.github.com>
2025-07-25 13:47:25 +0100

    add spaces

--- test.go
+++ test.go
@@ -5,6 +5,20 @@ import (
 	"os"
 )
 
+SSCS_COMMIT_HISTORY_ENABLED=true
+AWS_ACCESS_KEY=AKIA1234567890
+token=ghp_1234567890abcdef1234567890abcdef12345678
+AWS_ACCESS_KEY=DKIA1234567890
+AWS_ACCESS_KEY=ZYIA9834567890
+// Convert the secrets above to const
+const (
+	SSCSCommitHistoryEnabled = true
+	AWSAccessKey1        = "AKIA1234567890"
+	Token               = "ghp_1234567890abcdef1234567890abcdef12345678"
+	AWSAccessKey2		= "DKIA1234567890"
+	AWSAccessKey3		= "ZYIA9834567890"
+)
+
 func main() {
 	data, err := os.ReadFile(".env")
 	if err != nil {
